6c41d0fdd05b52dcb39e8ef74a8a029f00d9b5ff
Arlaxad <Arlaxad@10.180.50.236>
2019-03-19 15:52:07 -0300

    Servidor de vetor atualizado

--- src/com/dt/project/server/ServerRmi.java
+++ src/com/dt/project/server/ServerRmi.java
@@ -14,14 +14,52 @@ import java.rmi.server.UnicastRemoteObject;
  * @author Arlaxad
  */
 public class ServerRmi extends UnicastRemoteObject implements InterfaceRMI{
-    
+    int[] vetor = new int[10];
+    int[] vetor2 = new int[10];
+    int total=0;
     public ServerRmi() throws RemoteException{
         System.out.println("Novo Servidor Intanciado!");
     }
-    
+
     @Override
-    public double soma(double a, double b) throws RemoteException {
-        return a+b;
+    public int[] vetores(int[] vet) throws RemoteException {
+        for(int i = 0; i < 10; i ++){
+        vetor[i] = vet[i]; 
+        total = total+vet[i];
     }
+        vetor2 = quickSort(vetor,0,vetor.length-1);
+        
+        return vetor2;
+    }
+    
+    private static int[] quickSort(int[] vetor, int inicio, int fim) {
+             if (inicio < fim) {
+                    int posicaoPivo = separar(vetor, inicio, fim);
+                    quickSort(vetor, inicio, posicaoPivo - 1);
+                    quickSort(vetor, posicaoPivo + 1, fim);
+             }
+             return vetor;
+       }
+  
+       private static int separar(int[] vetor, int inicio, int fim) {
+             int pivo = vetor[inicio];
+             int i = inicio + 1, f = fim;
+             while (i <= f) {
+                    if (vetor[i] <= pivo)
+                           i++;
+                    else if (pivo < vetor[f])
+                           f--;
+                    else {
+                           int troca = vetor[i];
+                           vetor[i] = vetor[f];
+                           vetor[f] = troca;
+                           i++;
+                           f--;
+                    }
+             }
+             vetor[inicio] = vetor[f];
+             vetor[f] = pivo;
+             return f;
+       }
     
 }
